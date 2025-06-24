9ae3fd488595041c7f8f2bcae55b96a73a0f266d
Bodrul813 <mdbodrulislam813@gmail.com>
2025-06-24 17:16:08 +0600

    API Project create

---
+++ InfluPro/InfluPro.API/Controllers/SimpleControler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfluPro.API.Controllers
+{
+    [Route("api/[controller]")]  //api/controlar/action
+    [ApiController]
+    public class SimpleControler : ControllerBase
+    {
+        // HttpGet, HttpPost, HttpPut, HttpDelete, HttpPatch are action verbs
+        [HttpGet("getdemo1")]
+        public string GetDemo1()
+        {
+            return "Hello World, Welcome to API Project";
+        }
+
+        [HttpGet("getdemo2")]
+        public string GetDemo2()
+        {
+            return "Hello! Will you marry me!";
+        }
+    }
+}
